81c2eb4be8e01e0f48f22e70090cadeffd702e5f
loinsir <a9327370@gmail.com>
2023-10-01 14:16:33 +0900

    feat: implement AuroraImageWrapper

--- AuroraImage/Sources/AuroraImage/AuroraImage.swift
+++ AuroraImage/Sources/AuroraImage/AuroraImage.swift
@@ -1,2 +1,39 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
+//
+//  AuroraImageWrapper.swift
+//
+//
+//  Created by 김인환 on 10/1/23.
+//
+
+public struct AuroraImageWrapper<Base> {
+    public let base: Base
+    public init(base: Base) {
+        self.base = base
+    }
+}
+
+
+public protocol AuroraImageCompatible: AnyObject {
+    associatedtype AuroraImageBase
+    
+    static var ai: AuroraImageWrapper<AuroraImageBase>.Type { get set }
+    
+    var ai: AuroraImageWrapper<AuroraImageBase> { get set }
+}
+
+extension AuroraImageCompatible {
+    public static var ai: AuroraImageWrapper<Self>.Type {
+        get {
+            return AuroraImageWrapper<Self>.self
+        }
+        set { }
+    }
+    
+    public var ai: AuroraImageWrapper<Self> {
+        get {
+            return AuroraImageWrapper(base: self)
+        }
+        set { }
+    }
+}
+
